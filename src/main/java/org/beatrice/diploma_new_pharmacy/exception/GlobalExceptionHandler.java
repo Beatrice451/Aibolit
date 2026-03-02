79b917f1b0fb6db5a18f4769518e34541f2a94fe
BeatriceLaplan <beatricelaplan@gmail.com>
2026-03-02 21:19:58 +0300

    feat(exceptionHandler): обновлен глобальный обработчик исключений

--- src/main/java/org/beatrice/diploma_new_pharmacy/exception/GlobalExceptionHandler.java
+++ src/main/java/org/beatrice/diploma_new_pharmacy/exception/GlobalExceptionHandler.java
@@ -4,11 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.beatrice.diploma_new_pharmacy.auth.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // TOKEN EXCEPTIONS
     @ExceptionHandler({
             InvalidTokenException.class,
             RevokedTokenException.class,
@@ -27,6 +29,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
     }
 
+    // USER EXCEPTIONS
     @ExceptionHandler({
             UserAlreadyExistsException.class,
             PhoneAlreadyExistsException.class
@@ -44,6 +47,34 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 
+    // COOKIE EXCEPTIONS
+    @ExceptionHandler(MissingRequestCookieException.class)
+    public ResponseEntity<ErrorResponse> handleMissingCookieException(
+            Exception ex,
+            HttpServletRequest request
+    ) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.UNAUTHORIZED.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
 
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
+
+    // COMMON EXCEPTION HANLDER (FOR EVERYTHING THAT WAS NOT CAUGHT BY THE HANDLERS ABOVE)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(
+            Exception ex,
+            HttpServletRequest request
+    ) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
 }
 
