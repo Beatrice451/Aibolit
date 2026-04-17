6727270f74c84fa20a95703b035a01898713c953
BeatriceLaplan <beatricelaplan@gmail.com>
2026-04-17 23:41:32 +0300

    Что-то исправил, что-то сломал

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:1488',
+  baseURL: process.env.REACT_APP_API_URL || '/api',
 });
 
 const initAuth = () => {
